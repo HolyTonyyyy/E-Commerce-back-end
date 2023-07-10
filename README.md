ec1f820a5ecf07d9f60a4001505ce5c615a5fadb
Anthony Bradford <agb2152001@gmail.com>
2023-07-10 15:18:03 -0400

    commit

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # E-Commerce-back-end
 
-# i dont have the video to upload because it is not finish but i will resubmit once i do. until then some feedback just on the code will help
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,12 +5,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
+  Product.findAll()
+  .then(data => res.json(data)) 
+  .catch(err => res.json(err))
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findOne({
+     where:{
+      id: req.params.id
+     },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No Products found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
@@ -92,8 +111,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No Products found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
